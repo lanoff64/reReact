eac28cf58d8c9879ae46ff2dcbe9b2838a859960
lanoff64 <lanov64@gmail.com>
2021-11-02 00:47:57 +0600

    animation translaneX

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts,title,remove}) => {
 
@@ -13,9 +14,17 @@ const PostList = ({posts,title,remove}) => {
         <div>
             <div className="App">
                 <h1 style={{textAlign:'center'}}>{title}</h1>
+                <TransitionGroup>
                 {posts.map((post,index) =>
-                        <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
+                        <CSSTransition
+                            key={post.id}
+                            timeout={500}
+                            classNames="post">
+                        <PostItem remove={remove} number={index + 1} post={post}  />
+                        </CSSTransition>
                     )}
+
+                </TransitionGroup>
             </div>
         </div>
     );
